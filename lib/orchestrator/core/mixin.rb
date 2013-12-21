3baae4ddb6ffde580e8fcdb0e7d7a54eeb4684c7
Stephen von Takach <steve@cotag.me>
2013-12-21 21:29:43 +1100

    Added task support
    also a bunch of todos

--- lib/orchestrator/core/mixin.rb
+++ lib/orchestrator/core/mixin.rb
@@ -8,6 +8,22 @@ module Orchestrator
             def systems(name)
                 # TODO:: need to be able to look up systems via name or id
             end
+
+            def task(callback = nil, &block)
+                @__config__.thread.work(callback, &block)
+            end
+
+            def status
+                # TODO:: need to be able to save status variables
+            end
+
+            def subscribe(mod, status = nil, &callback)
+                # TODO:: need a subscription service
+            end
+
+            def unsubscribe(subscription)
+                
+            end
         end
     end
 end
