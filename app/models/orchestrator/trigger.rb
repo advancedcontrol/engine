6ae4655fd5eb83820177086e6d7b475f0ea40e61
Stephen von Takach <steve@cotag.me>
2015-05-25 23:46:33 +1000

    Add basic trigger validations

--- app/models/orchestrator/trigger.rb
+++ app/models/orchestrator/trigger.rb
@@ -1,3 +1,4 @@
+require 'set'
 
 module Orchestrator
     class Trigger < Couchbase::Model
@@ -30,5 +31,79 @@ module Orchestrator
         # -----------
         validates :name,       presence: true
         validates :conditions, presence: true
+
+        validate  :condition_list
+        validate  :action_list
+
+
+        KEYS = Set.new([
+            :equal, :not_equal, :greater_than, :greater_than_or_equal,
+            :less_than, :less_than_or_equal, :and, :or, :exclusive_or
+        ])
+        CONST_KEYS =  Set.new([:at, :cron])
+        def condition_list
+            if self.conditions
+                self.conditions.deep_symbolize_keys!
+
+                valid = true
+                self.conditions.each do |cond|
+                    if cond.length < 3
+                        valid = CONST_KEYS.include?(cond[0].to_sym)
+                    else
+                        valid = value?(cond[0]) && KEYS.include?(cond[1].to_sym) && value?(cond[2])
+                    end
+                    break if not valid
+                end
+
+                if not valid
+                    errors.add(:conditions, 'are not all valid')
+                end
+            end
+        end
+
+        STATUS_KEYS = Set.new([:mod, :index, :status, :keys])
+        # TODO:: Should also check types
+        def value?(val)
+            if val[:const]
+                # Should only store the constant
+                val.keep_if { |k, _| k == :const }
+                true
+            else
+                # Should be a status variable
+                val.keep_if { |k, _| STATUS_KEYS.include? k }
+                val[:index].is_a?(Fixnum) && val.has_key?(:mod) && val.has_key?(:status)
+            end
+        end
+
+
+        def action_list
+            if self.actions
+                self.actions.deep_symbolize_keys!
+
+                valid = true
+                self.actions.each do |act|
+                    valid = check_action(act)
+                end
+
+                if not valid
+                    errors.add(:actions, 'are not all valid')
+                end
+            end
+        end
+
+        ACTION_KEYS = Set.new([:type, :mod, :index, :func, :args])
+        def check_action(act)
+            act.keep_if { |k, _| ACTION_KEYS.include? k }
+
+            case act[:type].to_sym
+            when :exec
+                act[:index].is_a?(Fixnum) && act.has_key?(:mod) && act.has_key?(:func) && act[:args].is_a?(Array)
+            when :email
+                # TODO:: 
+                false
+            else
+                false
+            end
+        end
     end
 end
