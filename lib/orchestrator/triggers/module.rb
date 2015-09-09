a253a33e6fc9fad768d1796daeacfcf8bc3f3e72
Stephen von Takach <steve@cotag.me>
2015-09-09 11:38:33 +1000

    better support for keyword arguments

--- lib/orchestrator/triggers/module.rb
+++ lib/orchestrator/triggers/module.rb
@@ -213,7 +213,7 @@ module Orchestrator
                         case act[:type].to_sym
                         when :exec
                             # Execute the action
-                            logger.debug { "executing action #{act[:mod]}_#{act[:index]}.#{act[:func]}(#{act[:args]})" }
+                            logger.debug { "executing action #{act[:mod]}_#{act[:index]}.#{act[:func]}(#{act[:args].join(', ')})" }
                             system.get(act[:mod], act[:index]).method_missing(act[:func], *act[:args])
                         when :email
                             logger.debug { "sending email to: #{act[:to]}" }
