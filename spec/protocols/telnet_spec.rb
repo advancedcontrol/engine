d19243c8094dbd88f46b3017afeb2a04433e5e78
Stephen von Takach <steve@cotag.me>
2015-12-17 19:49:00 +1100

    add telnet protocol helper

---
+++ spec/protocols/telnet_spec.rb
@@ -0,0 +1,28 @@
+#encoding: ASCII-8BIT
+
+require 'rails'
+require 'protocols/telnet'
+
+
+describe "telnet protocol helper" do
+    before :each do
+        @log = []
+        @telnet = Protocols::Telnet.new do |cmd|
+            # Write callback
+            @log << cmd
+        end
+    end
+
+    it "should buffer input and process any telnet commands" do
+        @log << @telnet.buffer("\xFF\xFD\x18\xFF\xFD \xFF\xFD#\xFF\xFD'hello there")
+        expect(@log).to eq(["\xFF\xFC\x18", "\xFF\xFC ", "\xFF\xFC#", "\xFF\xFC'", "hello there"])
+    end
+
+    it "should append the appropriate line endings to requests" do
+        @telnet.buffer("\xFF\xFD\x18\xFF\xFD \xFF\xFD#\xFF\xFD'")
+        @log.clear
+
+        @telnet.write("hello")
+        expect(@log).to eq(["hello\r\n"])
+    end
+end
