1bb6da79eb0c48a50629aa494d3fd4ad4586b0cf
Stephen von Takach <steve@cotag.me>
2016-01-05 17:45:39 +1100

    add support for NTLM services

--- lib/orchestrator/utilities/constants.rb
+++ lib/orchestrator/utilities/constants.rb
@@ -180,6 +180,10 @@ module Orchestrator
                 @request[:keepalive] = !!val
             end
 
+            def ntlm_credentials(opts)
+                @config ||= {}
+                @config[:ntlm] = opts
+            end
 
         end
 
