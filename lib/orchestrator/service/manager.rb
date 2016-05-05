92c4550ad65ac994a82564eab197a9e3e520d51d
Stephen von Takach <steve@cotag.me>
2016-05-05 16:53:28 +1000

    (service:manager) slight change in load order
    Allows middleware to be applied in the on-load callback

--- lib/orchestrator/service/manager.rb
+++ lib/orchestrator/service/manager.rb
@@ -16,10 +16,11 @@ module Orchestrator
                 return true if @processor
 
                 @processor = Orchestrator::Device::Processor.new(self)
+                # Before super so we can apply middleware:
+                @connection = TransportHttp.new(self, @processor)
 
                 super online # Calls on load (allows setting of tls certs)
 
-                @connection = TransportHttp.new(self, @processor)
                 @processor.transport = @connection
                 true
             end
